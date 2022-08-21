5fc16672763fcf1b4fb07177f6009e6dd0b9ea82
hemcharan <hemcharan54@gmail.com>
2022-08-21 13:47:45 +0530

    ggf

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -80,7 +80,7 @@ const Hero = () => {
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Contact me or Drop a message
     </a>
   );
 
